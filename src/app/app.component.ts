2a50195af9867e326e076b1d7b0c1a13a5968296
Aliaksei Pliutsinski <aleksprutik@gmail.com>
2024-02-05 19:05:28 +0100

    feat: add counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngrx-for-beginners';
+  counter = 0;
+  updatedAt?: number;
+
+  get cannotDecrease(): boolean {
+    return this.counter <= 0;
+  }
+
+  increase(): void {
+    this.updatedAt = Date.now();
+    this.counter++;
+  }
+
+  decrease(): void {
+    this.updatedAt = Date.now();
+    this.counter--;
+  }
+
+  clear(): void {
+    this.updatedAt = Date.now();
+    this.counter = 0;
+  }
 }
